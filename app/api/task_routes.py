d6f0b270f7c78fd747164dcb3f72946866a608e9
Julie <juliebarreto897@gmail.com>
2023-06-24 11:29:56 -0500

    Working routes

--- app/api/task_routes.py
+++ app/api/task_routes.py
@@ -96,18 +96,3 @@ def update_task(taskId):
         return task.to_dict()
 
     return {'errors': validation_errors_to_error_messages(form.errors)}, 400
-
-
-
-@task_routes.route('/current', methods=['GET'])
-@login_required
-def get_current_task():
-    '''
-    Query for all tasks of current user and return them in a list of dictionaries
-    '''
-    current_user_id = session.get('user_id')
-    user = User.query.get(current_user_id)
-    if user is None:
-        return jsonify({'error': 'User not found'}), 404
-    else:
-        return jsonify(user.to_dict_with_tasks())
