30817b6bb8a70894cb60c675af19bad70a0d8570
Julie <juliebarreto897@gmail.com>
2023-06-24 22:45:37 -0500

    Delete Task by taskid successfully

--- app/api/task_routes.py
+++ app/api/task_routes.py
@@ -110,3 +110,20 @@ def get_current_task():
         return jsonify({'error': 'User not found'}), 404
     else:
         return jsonify(user.to_dict_with_tasks())
+
+@task_routes.route('/<int:taskId>', methods=['DELETE'])
+@login_required
+def delete_task(taskId):
+    '''
+    Deletes a specific task
+    '''
+    task = Task.query.get(taskId)
+    if task is None:
+        return jsonify({'error':'Task not found'}), 404
+    #check if the current user is the owner of the task
+    if task.user_id != current_user.id:
+        return jsonify({'error':'Unauthorized'}), 404
+    db.session.delete(task)
+    db.session.commit()
+
+    return jsonify({'messahe': 'Task successfully deleted'})
