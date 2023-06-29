f4001c7cd8eab5e290be6acf3488dc224a703f54
Julie <juliebarreto897@gmail.com>
2023-06-28 19:33:09 -0500

    added validations to Step1 successfully

--- react-app/src/components/Step1/index.js
+++ react-app/src/components/Step1/index.js
@@ -1,12 +1,28 @@
-import React from "react";
+import React, { useState } from "react";
 
 function Step1({location, setLocation, description, setDescription, handleNext, title, setTitle}){
+
+    const [errors, setErrors] = useState({})
+
+    const validate = () => {
+        const newErrors = {};
+        if (!location) newErrors.location = "Location is required";
+        if (!description) newErrors.description = "Description is required";
+        if (!title) newErrors.title = "Title is required";
+        return newErrors;
+    }
+
     const onNext = (e) => {
         e.preventDefault();
         // if(!location || description){
         //     alert('Both fields are required')
         //     return;
         // }
+        const result = validate();
+        if (Object.keys(result).length > 0){
+            setErrors(result)
+            return
+        }
         handleNext();
     }
     return (
@@ -20,6 +36,7 @@ function Step1({location, setLocation, description, setDescription, handleNext, 
                 required
                 />
             </label>
+            {errors.location && <p>{errors.location}</p>}
             <label>
                 Title:
                 <input
@@ -29,6 +46,7 @@ function Step1({location, setLocation, description, setDescription, handleNext, 
                 required
                 />
             </label>
+            {errors.title && <p>{errors.title}</p>}
             <label>
                 Description:
                 <textarea
@@ -37,6 +55,7 @@ function Step1({location, setLocation, description, setDescription, handleNext, 
                 required
                 />
             </label>
+            {errors.description && <p>{errors.description}</p>}
             <button onClick={onNext}>Next</button>
         </div>
     )
