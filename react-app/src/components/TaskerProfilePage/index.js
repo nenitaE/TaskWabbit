1f832510f73a5da9b310650731abc3c369319e68
Scot Nicol <scnicol@ucdavis.edu>
2023-07-01 15:12:14 -0700

    fixed the object issue with an indexing

--- react-app/src/components/TaskerProfilePage/index.js
+++ react-app/src/components/TaskerProfilePage/index.js
@@ -1,7 +1,7 @@
 import React, { useEffect, useState } from "react";
 import { NavLink, useHistory } from "react-router-dom/cjs/react-router-dom";
 import { useDispatch, useSelector } from "react-redux";
-import { getTaskerTaskTypes} from "../../store/taskerProfile";
+import { getTaskerTaskTypes } from "../../store/taskerProfile";
 import { getTaskTypes } from "../../store/taskTypes";
 import { getTaskers } from "../../store/taskers";
 import { getTasks } from "../../store/tasks";
@@ -14,20 +14,22 @@ function TaskerProfilePage() {
     const user = useSelector(state => state.session.user);
     //get tasktypes descriptions by tasktypeID from the state
     const taskTypes = useSelector(state => state.taskTypes);
+    const taskTypesById = {}
+    taskTypes.forEach(taskType => { taskTypesById[taskType.id] = taskType })
     console.log(taskTypes, "taskTypesfromState*************")
-    
+
     const dispatch = useDispatch();
     //get taskerProfile from the state
     let taskerProfile = useSelector(state => state.taskerProfile.taskerTaskTypes)
-    console.log (taskerProfile, "********TASKERprofile********")
+    console.log(taskerProfile, "********TASKERprofile********")
     // console.log (taskerProfile[0], "********CurrTASKERprofile********")
-    
+
     useEffect(() => {
         dispatch(getTaskers())
         dispatch(getTaskerTaskTypes())
         dispatch(getTaskTypes())
         dispatch(getTasks())
-        .then(() => setIsLoaded(true))
+            .then(() => setIsLoaded(true))
     }, [dispatch]);
 
     if (!taskerProfile) return null;
@@ -35,26 +37,28 @@ function TaskerProfilePage() {
     let currTaskerProfile = taskerProfile[0]
     // get list of current logged in tasker's tasktypes & hourlyrates by tasktypeID
     const currTaskTypesById = Object.values(currTaskerProfile.taskerTaskTypes);
-    console.log (currTaskTypesById, "********CURRTASKTYPESBYID********")
+    console.log(currTaskTypesById, "********CURRTASKTYPESBYID********")
 
-
+    let taskType;
     return (
         <div>
             <h1>This will be the Tasker Profile Page</h1>
             <h2>Your current tasktypes are</h2>
-            {isLoaded && currTaskTypesById.map((taskType) => (
-               <li>{taskTypes[taskType.id].type} at an hourly rate of ${taskType.hourlyRate}</li> 
+            {isLoaded && currTaskTypesById.map((taskerTaskType) => (
+                <li>
+                    {taskTypesById[taskerTaskType.taskType_id].type}
+                </li>
             ))}
-            
-        
+
+
             {!user || (
-                <span  className='create-new-tasktype'>
+                <span className='create-new-tasktype'>
                     <li>
                         <NavLink to="/taskerTaskTypes/new">Create a New Tasktype</NavLink>
                     </li>
                 </span>)}
-              
-            
+
+
         </div>)
 }
 
