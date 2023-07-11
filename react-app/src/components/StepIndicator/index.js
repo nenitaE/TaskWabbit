cd30cfa9b853f3b664f1297dccde1d61294b2cf9
Julie <juliebarreto897@gmail.com>
2023-07-11 12:13:24 -0500

    added CSS to StepIndicator

--- react-app/src/components/StepIndicator/index.js
+++ react-app/src/components/StepIndicator/index.js
@@ -1,19 +1,22 @@
 import "./StepIndicator.css"
+import React from "react"
 
-function StepIndicator({currentStep, onStepClick}){
+function StepIndicator({currentStep, onStepClick}) {
     return (
         <div className="step-indicator">
-            {[1,2,3,4].map((step) => (
-                <div key={step} className={`step${currentStep === step ? ' active' : ''}`}
-                onClick={() => onStepClick(step)}
-
-                >
-                    {step}
-                </div>
+            {[1,2,3,4].map((step, index) => (
+                <React.Fragment key={step}>
+                    {index > 0 && <div className={`step-line ${currentStep >= step ? 'active' : ''}`}/>}
+                    <div
+                        className={`step${currentStep >= step ? ' active' : ''}`}
+                        onClick={() => onStepClick(step)}
+                    >
+                        {step}
+                    </div>
+                </React.Fragment>
             ))}
         </div>
     )
-
 }
 
 export default StepIndicator
