0462770d5d972be51eb87f6793366e911733b16a
NenitaE <nenitaespinosa@gmail.com>
2023-07-11 22:37:52 -0400

    added code to versions

--- migrations/versions/20230711_215245_.py
+++ migrations/versions/20230711_215245_.py
@@ -7,7 +7,9 @@ Create Date: 2023-07-11 21:52:45.735732
 """
 from alembic import op
 import sqlalchemy as sa
-
+import os
+environment = os.getenv("FLASK_ENV")
+SCHEMA = os.environ.get("SCHEMA")
 
 # revision identifiers, used by Alembic.
 revision = 'b721a19e431a'
@@ -93,7 +95,9 @@ def upgrade():
     sa.ForeignKeyConstraint(['user_id'], ['users.id'], ),
     sa.PrimaryKeyConstraint('id')
     )
-    # ### end Alembic commands ###
+    if environment == "production":
+        op.execute(f"ALTER TABLE users SET SCHEMA {SCHEMA};")
+    # ### end Alembic commands ###qqqqqqqqq
 
 
 def downgrade():
