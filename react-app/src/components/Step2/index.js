a586a8dfdd724265eb425f675619049c6dd8e829
Julie <juliebarreto897@gmail.com>
2023-07-12 13:19:06 -0500

    Added some styling to step2

--- react-app/src/components/Step2/index.js
+++ react-app/src/components/Step2/index.js
@@ -4,6 +4,8 @@ import { useDispatch, useSelector } from "react-redux";
 import { authenticate } from '../../store/session';
 import { useParams } from "react-router-dom";
 import { getTasks } from '../../store/tasks';
+
+
 import './Step2.css'
 
 function Step2({ onStepComplete, taskers}){
@@ -11,6 +13,18 @@ function Step2({ onStepComplete, taskers}){
     const dispatch = useDispatch();
     const user = useSelector(state => state.session.user)
     const { taskTypeId } = useParams();
+
+    //Find the highest hourly rate from all taskers
+    let highestHourlyRate = 0;
+    taskers.forEach(tasker => {
+        tasker.taskerTaskTypes.forEach(taskType => {
+            if (taskType.taskType_id == taskTypeId && Number(taskType.hourlyRate) > highestHourlyRate) {
+                highestHourlyRate = Number(taskType.hourlyRate);
+            }
+        });
+    });
+    const [maxHourlyRate, setMaxHourlyRate] = useState(highestHourlyRate);
+
 
     const handleSelectTasker= (taskerId) => {
         const selectedTasker = filteredTaskers.find(tasker => tasker.id === taskerId);
@@ -76,26 +90,50 @@ function Step2({ onStepComplete, taskers}){
 
 
     return (
-        <div>
-            <label>
+        <div className='main_container'>
+            <div className='range-slider'>
+                <input
+                    type="range"
+                    min="0"
+                    max={highestHourlyRate}
+                    value={maxHourlyRate}
+                    onChange={event => setMaxHourlyRate(Number(event.target.value))}
+                />
+                {maxHourlyRate}$
+            </div>
+
+            <div className='taskers'>
+            {/* <label> */}
                 Choose your tasker:
                     {filteredTaskers.length > 0 ? (
-                        filteredTaskers.map((tasker) => (
-                            <div key={tasker.id}>
-                                <div>
-                                    <h2>{tasker.firstName}</h2>
-                                    <p>{tasker.taskerTaskTypes.find(taskType => taskType.taskType_id == taskTypeId).hourlyRate}/hr</p>
+                        filteredTaskers
+                        .filter(tasker => {
+                            const hourlyRate = Number(tasker.taskerTaskTypes.find(taskType => taskType.taskType_id == taskTypeId).hourlyRate);
+                            return hourlyRate <= maxHourlyRate;
+                        })
+                        .map((tasker) => (
+                            <div key={tasker.id} className='tasker-card'>
+                                <div className='tasker-image-container'>
+                                    <img src="https://placehold.it/100" alt='Profile' className='profile-image'></img>
+                                    <button onClick={() => handleSelectTasker(tasker.id)}>Select and continue</button>
                                 </div>
-                                <p>({tasker.reviews.length} reviews)</p>
-                                <p>Tasks done: {countTaskerTasks(tasker.id, taskTypeId)}</p>
-                                <p>{tasker.reviews[0] ? tasker.reviews[0].description : 'No reviews'}</p>
-                                <button onClick={() => handleSelectTasker(tasker.id)}>Select and continue</button>
+                                <div className='tasker-info-container'>
+                                    <div className='header'>
+                                        <h2>{tasker.firstName}</h2>
+                                        <h2>{tasker.taskerTaskTypes.find(taskType => taskType.taskType_id == taskTypeId).hourlyRate}/hr</h2>
+                                    </div>
+
+                                    <p>({tasker.reviews.length} reviews)</p>
+                                    <p>Tasks done: {countTaskerTasks(tasker.id, taskTypeId)}</p>
+                                    <p>{tasker.reviews[0] ? tasker.reviews[0].description : 'No reviews'}</p>
+                                </div>
                             </div>
                         ))
                     ) : (
                         <p>No taskers available</p>
                     )}
-            </label>
+            {/* </label> */}
+            </div>
             <button type="button" onClick={handleBack}>
                 Back
             </button>
