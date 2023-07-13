a37d4542b9fc54f7b3042411ca407524a057e02c
Scot Nicol <scnicol@ucdavis.edu>
2023-07-13 11:40:50 -0700

    css trying to make signuplogin look nice

--- react-app/src/components/LoginSignup/LoginSignup.js
+++ react-app/src/components/LoginSignup/LoginSignup.js
@@ -6,23 +6,34 @@ import { useSelector } from "react-redux";
 import { Redirect, useParams } from "react-router-dom";
 
 
+
+
 function LoginSignup() {
     const { taskTypeId } = useParams();
     const sessionUser = useSelector((state) => state.session.user);
     if (sessionUser) return <Redirect to={`/tasks/new/${taskTypeId}`} />;
 
     return (
-        <div>
-            <OpenModalButton
-                buttonText="Signup"
-                modalComponent={<SignupFormModal/>}
-                onModalClose={<Redirect to={`/tasks/new/${taskTypeId}`} />}
-            />
-            <OpenModalButton
-                buttonText="Login"
-                modalComponent={<LoginFormModal/>}
-                onModalClose={<Redirect to={`/tasks/new/${taskTypeId}`} />}
-            />
+        <div className="Login_Signup_container">
+            <div>taskwabbit</div>
+            <div>
+                <OpenModalButton
+                    buttonText="Signup"
+                    modalComponent={<SignupFormModal />}
+                    onModalClose={<Redirect to={`/tasks/new/${taskTypeId}`} />}
+                />
+            </div>
+            <div>
+                <OpenModalButton
+                    buttonText="Login"
+                    modalComponent={<LoginFormModal />}
+                    onModalClose={<Redirect to={`/tasks/new/${taskTypeId}`} />}
+                />
+            </div>
+            <div>
+                By signing up you agree to our Terms of Use and Privacy Policy.
+            </div>
+
         </div>
     )
 }
