2b0bff023232a78a4f122a11d841ae5c2ce7997a
Scot Nicol <scnicol@ucdavis.edu>
2023-07-13 12:17:06 -0700

    added background picture and inner signup login menu

--- react-app/src/components/LoginSignup/LoginSignup.js
+++ react-app/src/components/LoginSignup/LoginSignup.js
@@ -4,7 +4,7 @@ import SignupFormModal from "../SignupFormModal";
 import LoginFormModal from "../LoginFormModal";
 import { useSelector } from "react-redux";
 import { Redirect, useParams } from "react-router-dom";
-
+import "./loginSignup.css"
 
 
 
@@ -14,26 +14,29 @@ function LoginSignup() {
     if (sessionUser) return <Redirect to={`/tasks/new/${taskTypeId}`} />;
 
     return (
-        <div className="Login_Signup_container">
-            <div>taskwabbit</div>
-            <div>
-                <OpenModalButton
-                    buttonText="Signup"
-                    modalComponent={<SignupFormModal />}
-                    onModalClose={<Redirect to={`/tasks/new/${taskTypeId}`} />}
-                />
+        <div className="login-signup-root">
+            <div className="login-signup-container">
+                <div className="login-signup-inner">
+                    <div className="taskwabbit-title">taskwabbit</div>
+                    <div>
+                        <OpenModalButton
+                            buttonText="Signup"
+                            modalComponent={<SignupFormModal />}
+                            onModalClose={<Redirect to={`/tasks/new/${taskTypeId}`} />}
+                        />
+                    </div>
+                    <div>
+                        <OpenModalButton
+                            buttonText="Login"
+                            modalComponent={<LoginFormModal />}
+                            onModalClose={<Redirect to={`/tasks/new/${taskTypeId}`} />}
+                        />
+                    </div>
+                    <div>
+                        By signing up you agree to our Terms of Use and Privacy Policy.
+                    </div>
+                </div>
             </div>
-            <div>
-                <OpenModalButton
-                    buttonText="Login"
-                    modalComponent={<LoginFormModal />}
-                    onModalClose={<Redirect to={`/tasks/new/${taskTypeId}`} />}
-                />
-            </div>
-            <div>
-                By signing up you agree to our Terms of Use and Privacy Policy.
-            </div>
-
         </div>
     )
 }
