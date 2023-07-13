ca69da5f29b3600b44d4c9d1c86ba4fcb4db6cd4
root <root@Im-Laptop.localdomain>
2023-07-13 10:05:03 -0400

    fixing bug, after logging out and hitting back brower shows old user data

--- react-app/src/components/UpdateReview/index.js
+++ react-app/src/components/UpdateReview/index.js
@@ -3,21 +3,34 @@ import { useDispatch, useSelector } from "react-redux"
 import { useParams } from "react-router-dom/cjs/react-router-dom.min";
 import { getRevById } from "../../store/reviews";
 import UpdateReview from "./UpdateReview";
+import { useHistory } from "react-router-dom/cjs/react-router-dom";
 
 
 const Update = () => {
     const dispatch = useDispatch();
+    const history = useHistory()
 
     const {id} = useParams()
     // console.log(id, 'test')
     const review = useSelector(state => state.reviewReducer[id])
-    // console.log(review, 'review update')
+
+    const userSession = useSelector(state => state.session.user)
+    console.log(review, userSession, 'review update')
 
     useEffect(() => {
 
         dispatch(getRevById(id))
         // console.log(id, 'idTest')
-    }, [dispatch])
+    }, [dispatch, userSession])
+
+    if(!userSession || userSession.id !== review.user_id){
+        return(
+            <>
+                {alert('Resticted')}
+                {history.push('/')}
+                </>
+            )
+        }
 
     if(!review){
         return(
