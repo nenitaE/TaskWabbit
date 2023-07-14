6477f673b524bedddbcc7f59184079d659a1c129
Julie <juliebarreto897@gmail.com>
2023-07-13 20:58:45 -0500

    basic structure of pasttaskpage

--- react-app/src/components/PastTasks/index.js
+++ react-app/src/components/PastTasks/index.js
@@ -30,35 +30,44 @@ function PastTasksPage(){
     }
 
     return (
-        <div>
-        <h1>Past Tasks</h1>
-        <Link to="/tasks/current">Current Tasks</Link>
-        <Link to="/tasks/past">Past Tasks</Link>
-        {tasks && tasks
-        .filter(task => new Date(task.task_date) < new Date()) //filter to past tasks to show only tasks with date < today
-        .map(task => {
-            const taskDate = new Date(task.task_date);
-            const currentDate = new Date();
-            currentDate.setHours(0,0,0,0); // set current time to 00:00:00
-            const uniqueKey = `${task.id}_`;
-            const tasker = taskers.find(tasker => tasker.id === task.tasker_id)
+        <div className="main-container">
+            <div className="link-container">
+            <h1>Past Tasks</h1>
+                <Link to="/tasks/current">Current Tasks</Link>
+                <Link to="/tasks/past">Past Tasks</Link>
+            </div>
+            <div className="tasks-container">
+                {tasks && tasks
+                .filter(task => new Date(task.task_date) < new Date()) //filter to past tasks to show only tasks with date < today
+                .map(task => {
+                    const taskDate = new Date(task.task_date);
+                    const currentDate = new Date();
+                    currentDate.setHours(0,0,0,0); // set current time to 00:00:00
+                    const uniqueKey = `${task.id}_`;
+                    const tasker = taskers.find(tasker => tasker.id === task.tasker_id)
 
-            return (
-                <div key={uniqueKey} className="task-card">
-                    {task.taskType && <h2>{task.taskType.type}</h2>}
-                    {tasker && <p>{tasker.firstName}</p>}
-                    <p>{task.title}</p>
-                    <p>Date: {task.task_date}</p>
-                    <p>Location: {task.location}</p>
-
-                    {/* <p>Id: {task.tasker_id}</p> */}
-
-                    <button onClick={() => openDeleteModal(task.id)}>Delete Task</button>
-                    <CreateReviewModal tasker_id={task.tasker_id}/>
-                    {taskDate >= currentDate && <Link to={`/tasks/${task.id}/edit`}>Edit Task</Link>}
-                </div>
-            )
-        })}
+                    return (
+                        <div key={uniqueKey} className="task-card">
+                            <div className="task-type">
+                                {task.taskType && <h2>{task.taskType.type}</h2>}
+                            </div>
+                            <div className="tasker-name">
+                                {tasker && <h2>{tasker.firstName}</h2>}
+                            </div>
+                            <div className="tasktitle-date-location">
+                                <p>{task.title}</p>
+                                <p>Date: {task.task_date}</p>
+                                <p>Location: {task.location}</p>
+                            </div>
+                            <div className="task-button-actions">
+                                <button onClick={() => openDeleteModal(task.id)}>Delete Task</button>
+                                <CreateReviewModal tasker_id={task.tasker_id}/>
+                                {taskDate >= currentDate && <Link to={`/tasks/${task.id}/edit`}>Edit Task</Link>}
+                            </div>
+                        </div>
+                    )
+                })}
+        </div>
     </div>
 )
 }
